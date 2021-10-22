0939b4ffef3ec79b62ce0e290b003b5d4a2597c0
David Cuy <david.cuy.sanchez@gmail.com>
2021-10-22 17:36:18 -0500

    feature: quiz create view completed

--- code/educational-system/src/app/pages/quiz/quiz-create/quiz-create.component.ts
+++ code/educational-system/src/app/pages/quiz/quiz-create/quiz-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { QuizService } from '../../../services/quiz.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quiz-create',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiz-create.component.css']
 })
 export class QuizCreateComponent implements OnInit {
+  name: string = null;
+  description: string = null;
 
-  constructor() { }
+  constructor(public quizService: QuizService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  saveQuiz(): void {
+    if (this.name === null || this.description === null) {
+      alert('Toos los campos deben estar llenados');
+      return;
+    }
+
+    this.quizService.create({
+      Name: this.name,
+      Description: this.description
+    }).subscribe(resp => {
+      if (resp?.IdQuiz) {
+        this.router.navigate([`quiz/${resp?.IdQuiz}/question/create`]);
+      }
+    });
+
   }
 
 }
